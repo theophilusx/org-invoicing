bf77e57260124d63ab201d6488ce508626b71f4e
Tim Cross <theophilusx@gmail.com>
2020-11-15 16:30:14 +1100

    Updae invoice generation functions

--- src/invoicing.el
+++ src/invoicing.el
@@ -1,9 +1,15 @@
 (defvar oi-company '((:name . "Fred Flintstone")
-                     (:addr1 . "88 Boulder Av.")
-                     (:addr2 . "Bedrock 2350")
+                     (:address1 . "88 Boulder Av.")
+                     (:address2 . "Bedrock 2350")
                      (:email . "fred@claytablet.com")
                      (:phone . "5555 555 555"))
-  "Contractor company details.")
+      "Contractor company details.")
+
+(defvar oi-invoice-class "traditional-article"
+  "The Latex class to use when generating invoices.")
+
+(defvar oi-invoice-style (expand-file-name "~/.spacemacs.d/invoice/invoice")
+  "The Latex style file to use when creating invoices.")
 
 
 (defun oi-next-invoice-number ()
@@ -36,13 +42,73 @@ current value for last invoice number."
   (save-excursion
     (beginning-of-buffer)
     (let ((top (search-forward "* Client")))
-      (list (list :name  (org-entry-get top "TITLE"))
-            (list :addr1 (org-entry-get top "Address1"))
-            (list :addr2 (org-entry-get top "Address2"))
-            (list :addr3 (org-entry-get top "Address3"))
-            (list :phone (org-entry-get top "Phone"))
-            (list :email (org-entry-get top "Emaiil"))
-            (list :rate (string-to-number (org-entry-get top "Rate")))))))
+      (list (cons :name  (org-entry-get top "Name"))
+            (cons :address1 (org-entry-get top "Address1"))
+            (cons :address2 (org-entry-get top "Address2"))
+            (cons :address3 (or (org-entry-get top "Address3") ""))
+            (cons :phone (org-entry-get top "Phone"))
+            (cons :email (org-entry-get top "Emaiil"))
+            (cons :rate (string-to-number (org-entry-get top "Rate")))
+            (cons :terms (org-entry-get top "Terms"))))))
+
+(defun oi-insert-invoice-header (ino)
+  (insert (format "#+TITLE: %s\n" ino))
+  (insert "#+OPTIONS: toc:nil title:nil num:nil\n"
+          (format "#+LATEX_CLASS: %s\n" oi-invoice-class)
+          "#+LATEX_HEADER: \\usepackage{array}\n"
+          "#+LATEX_HEADER: \\usepackage{tabularx}\n"
+          (format "#+LATEX_HEADER: \\usepackage{%s}\n\n" oi-invoice-style)))
+
+(defun oi-insert-company-header ()
+  (insert "#+EXPORT_LATEX:\n"
+          "  \\hfil{\\Huge\\bf Invoice}\\hfil\\bigskip\\break\\hrule\n"
+          (format "  %s \\hfill %s \\newline\n"
+                  (cdr (assoc :address1 oi-company))
+                  (cdr (assoc :name oi-company)))
+          (format "  %s \\hfill %s \\newline\n"
+                  (cdr (assoc :address2 oi-company))
+                  (cdr (assoc :email oi-company)))
+          (format "  %s \\hfill %s \\newline\n\\newline\n"
+                  (or (cdr (assoc :address3 oi-company)) "")
+                  (cdr (assoc :phone oi-company)))
+          "#+END_EXPORT:\n\n"))
+
+(defun oi-insert-client-header (client)
+  (insert "#+EXPORT_LATEX:\n"
+              "  \\begin{tabularx}{\\textwidth}{l X r}\n"
+              "    \\begin{tabular}{l}\n"
+              "      \\textbf{Invoice to} \\\\\n"
+              (format "      \\ %s \\\\\n" (cdr (assoc :name client)))
+              (format "      \\ %s \\\\\n" (cdr (assoc :address1 client)))
+              (format "      \\ %s \\\\\n" (cdr (assoc :address2 client)))
+              (format "      \\ %s \\\\\n" (cdr (assoc :address3 client)))
+              "    \\end{tabular}\n"
+              "    & \\ &\n"
+              "    \\begin{tabular}{|c|c|}\n"
+              "      \\hline\n"
+              "      \\textbf{Date} & \\textbf{Invoice \\#} \\\\\n"
+              "      \\hline\n"
+              (format "      \\today & %s \\\\\n" invoice-number)
+              "      \\hline\n"
+              "      \\textbf{Terms} & \\textbf{Due Date} \\\\\n"
+              "      \\hline\n"
+              (format "      %1$s & \\AdvanceDate[%1$s]\\today \\\\\n"
+                      (cdr (assoc :terms client)))
+              "     \\hline\n"
+              "    \\end{tabular}\n"
+              "  \\end{tabularx}\n"
+              "#+END_EXPORT:\n\n"))
+
+(defun oi-create-invoice (invoice-number client period path)
+  (let ((inv-file (concat path invoice-number ".org")))
+    (save-current-buffer
+      (find-file inv-file)
+      (beginning-of-buffer)
+      (oi-insert-invoice-header invoice-number)
+      (oi-insert-company-header)
+      (oi-insert-client-header client)
+      (save-buffer)
+      inv-file)))
 
 (defun oi-make-new-invoice-header ()
   (interactive)
@@ -55,10 +121,13 @@ current value for last invoice number."
     (insert (format "%s\n\n" inv))
     (let* ((period (oi-invoice-period))
           (org-clock-clocktable-default-properties (append '(:scope file :maxlevel 3)
-                                                           period)))
-      (message "CLock table props: %s" org-clock-clocktable-default-properties)
+                                                           period))
+          (client-path (file-name-directory (buffer-file-name)))
+          (i-file (oi-create-invoice inv client period client-path)))
       (insert (format "   Invoice: %s\n" inv))
       (insert (format "   Period %s to %s\n\n"
                       (plist-get period :tstart)
                       (plist-get period :tend)))
+      (insert (format "   ORG: [[file:%s][%s]]\n" i-file inv))
+      (insert (format "   PDF: [[file:%s%2$s.pdf][%2$s]]\n\n" client-path inv))
       (org-dynamic-block-insert-dblock "clocktable"))))
